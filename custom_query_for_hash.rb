b02f33c26569bc01413d579b418698c85a07eca7
Yen168 <yenan.chen@baruchmail.cuny.edu>
2015-05-30 21:20:04 -0400

    Create custom_query_for_hash.rb

---
+++ custom_query_for_hash.rb
@@ -0,0 +1,39 @@
+
+require 'csv'
+require './person'
+
+people = []
+hash_save = []
+
+CSV.foreach("input.csv", headers: true,header_converters: :symbol) do |row|
+   hash_save.push(row.to_hash)
+   
+   people.push(Person.new(row.to_hash))
+   
+end
+
+
+search = [year: "1999"]
+search2 = [year: "1999", :model=>"T210"]
+
+
+
+def hashQ(hash_save,search)
+	
+	result_list = []
+	hash_save.find_all do |h|
+		
+		result = true
+
+		search.each {|hh| hh.each {|k,v| result = false if h[k] !=v}}
+		result_list.push(h) if result != false
+
+	end
+	result_list
+end
+
+k = hashQ(hash_save,search)
+puts k
+
+k2 = hashQ(hash_save,search2)
+puts k2
